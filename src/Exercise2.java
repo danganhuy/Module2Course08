32980d3c65d11938313be0fe1dffd19ee758d91a
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-11-12 16:29:23 +0700

    Exercise 2 finished

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,2 +1,10 @@
+import myClass.FizzBuzzTest;
+
 public class Exercise2 {
+    public static void main(String[] args) {
+        System.out.println(FizzBuzzTest.fizzBuzz(5));
+        System.out.println(FizzBuzzTest.fizzBuzz(3));
+        System.out.println(FizzBuzzTest.fizzBuzz(15));
+        System.out.println(FizzBuzzTest.fizzBuzz(17));
+    }
 }
